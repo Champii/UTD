1ca78f1e216363224f8e6f59cac2d24a30294faa
Romain Richard <bigzounet@hotmail.com>
2014-04-30 23:34:17 +0200

    monsters are moving
    add monster movement and assets

---
+++ Assets/Scripts/MonsterMove.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var endPoint : Vector3;
+var duration : float = 1.0;
+ 
+private var startPoint : Vector3;
+private var startTime : float;
+private var route = new Array(Vector3(10,0,10), Vector3(-10,0,-10));
+private var actualPosition : int=-1;
+ 
+function Start() {
+    startPoint = transform.position;
+    startTime = Time.time;
+}
+ 
+function Update () {
+
+	if (actualPosition < (route.length - 1))
+	{
+		if (route[actualPosition + 1] == transform.position) {
+			startTime = Time.time;
+			actualPosition++;
+		}
+		else {
+			var position = (actualPosition == -1) ? startPoint : route[actualPosition];
+			transform.position = Vector3.Lerp(position, route[actualPosition + 1], 
+			(Time.time - startTime) / duration);
+    		animation.Play("walk");
+		}
+	}
+	else {
+		animation.Play("death");
+		Destroy(this);
+		Destroy(gameObject, 1);
+	}
+}

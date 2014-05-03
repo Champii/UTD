37111bb5006aeadd0620a565869544e30102b994
Champii <champii.akronym@gmail.com>
2014-05-03 18:56:41 +0200

    missing bullet.js

---
+++ Assets/Scripts/Bullet.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var target : GameObject;
+
+function Start () {
+//	Debug.Log(target);
+}
+
+function Update () {
+//	Debug.Log(target.transform.position);
+	transform.position = Vector3.Lerp(transform.position, target.transform.position, 0.1);
+//	Debug.Log(target);
+}
+function OnEnterCollision(other : Collision) {
+	Debug.Log("COLLISIONNNNNNN");
+	Debug.Log(other);
+}
